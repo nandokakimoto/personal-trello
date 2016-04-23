0e0dccb42ab7fcb11ba907624ac1a40f40fa0fd5
Fernando Kakimoto <nandokakimoto@gmail.com>
2016-04-23 15:47:06 -0300

    Setting port number based on environment.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 // Prepare app to bootstrap and jquery
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
@@ -14,13 +16,17 @@ app.use(less(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
 // Prepare app to use pug template engine
-app.set('views', './views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res){
   res.render('home');
 });
 
-app.listen(3000, function(){
-  console.log('Server is listening on port 3000...');
+// Custom handlers
+require('./signin')(app);
+require('./signup')(app);
+
+app.listen(app.get('port'), function(){
+  console.log('Server is listening on port', app.get('port'));
 });
