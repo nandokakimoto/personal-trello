9b03aee63a000dd2631aae4ca3c9dec850b125ba
Fernando Kakimoto <nandokakimoto@gmail.com>
2016-06-14 23:04:25 -0300

    Split signin signup angular controllers.

---
+++ public/js/controllers/signin.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('customTrello')
+  .controller('SigninCtrl', function($scope, $http){
+    $scope.user = {
+      email: 'nandokakimoto@gmail.com',
+      password: '12345678'
+    };
+
+    $scope.submitForm = function(){
+      $http({
+        method: 'POST',
+        url: '/auth/session',
+        data: $scope.user
+      }).then(
+        function success(response){
+          alert('success');
+        },
+        function error(response){
+          alert('error');
+        }
+      );
+    };
+  });
--- public/js/controllers/signup.js
+++ public/js/controllers/signup.js
@@ -23,25 +23,4 @@ angular.module('customTrello')
         }
       );
     };
-  })
-  .controller('SigninCtrl', function($scope, $http){
-    $scope.user = {
-      email: 'nandokakimoto@gmail.com',
-      password: '12345678'
-    };
-
-    $scope.submitForm = function(){
-      $http({
-        method: 'POST',
-        url: '/users/signin',
-        data: $scope.user
-      }).then(
-        function success(response){
-          alert('success');
-        },
-        function error(response){
-          alert('error');
-        }
-      );
-    };
   });
