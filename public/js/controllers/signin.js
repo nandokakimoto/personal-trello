f033fb6792c9ae494a24288fff76922aa064b76e
Fernando Kakimoto <nandokakimoto@gmail.com>
2016-10-04 23:16:42 -0300

    Displays error message if authentication fails (issue #1).

--- public/js/controllers/signin.js
+++ public/js/controllers/signin.js
@@ -3,13 +3,15 @@
 angular.module('personalTrello')
   .controller('SigninCtrl', function($scope, $location, $http, Auth){
     $scope.user = {};
+    $scope.errorMessage = "";
 
     $scope.submitForm = () => {
+      $scope.errorMessage = "";
       Auth.login($scope.user, (err) => {
         if (!err) {
           $location.path('/');
         } else {
-          alert('error');
+          $scope.errorMessage = "Invalid credentials. Please, try again.";
         }
       });
     };
